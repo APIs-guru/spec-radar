305afcb8e0b1bb10646d7122cd9825130444b4fb
Roman Hotsiy <gotsijroman@gmail.com>
2016-09-15 15:08:25 -0400

    Links

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,8 +1,32 @@
 'use strict';
 
+const LINKS = {
+  'swagger_1': [
+    { title: 'SwaggerUI', urlTmpl: 'http://petstore.swagger.io/?url=https://crossorigin.me/{link}' },
+  ],
+  'swagger_2': [
+    { title: 'SwaggerUI', urlTmpl: 'http://petstore.swagger.io/?url={link}' },
+  ]
+}
+
+
 chrome.runtime.onMessage.addListener(function (msg, sender, response) {
+  if (msg.subject !== 'data') return;
   let $buttons = document.getElementById('buttons');
-  $buttons.innerText = JSON.stringify(msg.data);
+  let type = msg.data.type;
+  let url = msg.data.url;
+  let links = LINKS[type];
+  if (!links) links = [];
+
+
+
+  for (let link of links) {
+    let $aTag = document.createElement('a');
+    $aTag.setAttribute('href', link.urlTmpl.replace('{link}', url));
+    $aTag.setAttribute('target', '_blank');
+    $aTag.innerHTML = link.title;
+    $buttons.appendChild($aTag);
+  }
 });
 
 chrome.runtime.sendMessage({from: 'popup', subject: 'getData'});
